16b9ae4e3e2af6dacb213268a6979d31ff04bda2
Jacob Williamson <jacobwilliamson@Jacobs-MacBook-Air.local>
2018-01-10 17:02:48 -0500

    Merged Measurement.swift to Ingredient.swift

--- Fork/Ingredient.swift
+++ Fork/Ingredient.swift
@@ -8,6 +8,129 @@
 
 import Foundation
 
-class Ingredient{
-
+enum scheme{
+    case standard
+    case metric
 }
+
+//Metric Units
+enum mUnit{
+    case none
+    case mL
+    case L
+    case whole
+    case g
+    case kg
+    
+}
+
+enum allergen{
+    case milk
+    case eggs
+    case peanuts
+    case treenuts
+    case soy
+    case gluten
+    case fish
+    case shellfish
+}
+
+//Standard Units
+enum sUnit{
+    case none
+    case tablespoon
+    case teaspoon
+    case cup
+    case pint
+    case gallon
+    case whole
+    case ounce //Dry Ounce not Fluid Ounce
+    case pound
+}
+
+class Ingredient{
+    var name: String = ""
+    var metricAmount: Double = 0
+    var metricUnit: mUnit = mUnit.none
+    var standardAmount: Double = 0
+    var standardUnit: sUnit = sUnit.none
+    var currentScheme: scheme
+    var allergens: [allergen] = []
+    
+    init(mAmount: Double, mUnits: mUnit, name: String, allergens: [allergen]){
+        metricAmount = mAmount
+        metricUnit = mUnits
+        currentScheme = .metric
+        convertUnits()
+    }
+    
+    init(sAmount: Double, sUnits: sUnit, name: String, allergens: [allergen]){
+        standardAmount = sAmount
+        standardUnit = sUnits
+        currentScheme = .standard
+        convertUnits()
+    }
+    
+    func convertUnits(){
+        switch self.currentScheme{
+        case .standard:
+            switch standardUnit{
+            case .teaspoon: // teaspoons ---> milliliters
+                metricAmount = standardAmount * 4.9289166908532360267
+                metricUnit = .mL
+                break
+            case .tablespoon: // tablespoons ---> milliliters
+                metricAmount = standardAmount * 14.786754853806497678
+                metricUnit = .mL
+                break
+            case .cup: // cups ---> milliliters
+                metricAmount = standardAmount * 236.58807766090396285
+                metricUnit = .mL
+                break
+            case .pint: // pints ---> liters
+                metricAmount = standardAmount * 0.47317615532180790083
+                metricUnit = .L
+                break
+            case .gallon: // gallons ---> liters
+                metricAmount = standardAmount * 3.7854092425744632067
+                metricUnit = .L
+                break
+            case .ounce: // dry ounces ---> grams
+                metricAmount = standardAmount * 28.349500000010491
+                metricUnit = .g
+                break
+            case .pound: // pounds ---> kilograms
+                metricAmount = standardAmount * 0.453592
+                metricUnit = .kg
+                break
+            default:
+                break
+            }
+            self.currentScheme = .metric
+        case .metric:
+            switch metricUnit{
+            case .mL: // milliliters ---> cups
+                standardAmount = metricAmount * 0.0042267500000001054228
+                standardUnit = .cup
+                break
+            case .L: // liters ---> pints
+                standardAmount = metricAmount * 2.11337500000005285
+                standardUnit = .pint
+                break
+            case .g: // grams ---> ounces
+                standardAmount = metricAmount * 0.035274
+                standardUnit = .ounce
+                break
+            case .kg: // kilograms ---> pounds
+                standardAmount = metricAmount * 2.20462
+                standardUnit = .pound
+                break
+            default:
+                break
+            }
+            self.currentScheme = .standard
+        }
+    }
+}
+
+
