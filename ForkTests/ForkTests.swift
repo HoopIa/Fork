41445733b3821fa867fd5f8ad714bcd033b156d8
Jacob Williamson <jacobwilliamson@Jacobs-MacBook-Air.local>
2018-01-10 11:35:14 -0500

    Initial Commit of My Dreams

---
+++ ForkTests/ForkTests.swift
@@ -0,0 +1,36 @@
+//
+//  ForkTests.swift
+//  ForkTests
+//
+//  Created by Jacob Williamson on 1/10/18.
+//  Copyright © 2018 Jacob Williamson. All rights reserved.
+//
+
+import XCTest
+@testable import Fork
+
+class ForkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
