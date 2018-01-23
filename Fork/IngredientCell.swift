9c2e44a42d6e094a921c3f9562d5f1baa18025e7
Jacob Williamson <jacobwilliamson@Jacobs-MacBook-Air.local>
2018-01-23 18:29:53 -0500

    Updated Skeleton of RecipeViewController

---
+++ Fork/IngredientCell.swift
@@ -0,0 +1,24 @@
+//
+//  IngredientCell.swift
+//  Fork
+//
+//  Created by Jacob Williamson on 1/23/18.
+//  Copyright © 2018 Jacob Williamson. All rights reserved.
+//
+
+import UIKit
+
+class IngredientCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
